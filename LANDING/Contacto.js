b7e170860be1e4b82727d78d8f301ac43606bc9e
MiloschMarkoPerezMendoza <106850699+MiloschMarkoPerezMendoza@users.noreply.github.com>
2023-06-07 02:24:34 -0500

    Delete LANDING directory

--- LANDING/Contacto.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const btnEnviar = document.getElementById('btnEnviar');
-    btnEnviar.addEventListener('click', mostrarMensaje);
-
-    function mostrarMensaje() {
-        alert("Datos enviados");
-    }
-});
